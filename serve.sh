2ca9d6d4d5019aff220e0c9d3b35faab141edc8f
Dominic Watson <watson.dominic@gmail.com>
2015-03-29 23:21:29 +0100

    Using a lucee logo for trayicon when running serve.sh

--- serve.sh
+++ serve.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 CWD="$(dirname $(readlink -f $0))"
-box start name=luceedocslocalserver directory=$CWD/server/ force=true rewritesEnable=true rewritesConfig=$CWD/server/urlrewrite.xml port=4040
+box start name=luceedocslocalserver directory=$CWD/server/ force=true rewritesEnable=true rewritesConfig=$CWD/server/urlrewrite.xml port=4040 trayIcon=$CWD/luceelogoicon.png
