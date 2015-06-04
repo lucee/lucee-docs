62f3d560a7a4b8916e763ac4a7f8c00b01204212
Dominic Watson <watson.dominic@gmail.com>
2015-06-04 22:11:17 +0100

    Creating travis build specific section of the build.sh to do deployent bits

--- build.sh
+++ build.sh
@@ -2,5 +2,19 @@
 
 cd `dirname $0`
 CWD="`pwd`"
-
+echo "Building documentation with Lucee :)"
 box $CWD/build.cfm
+echo "Building complete"
+if [[ $TRAVIS_BRANCH == 'master' ]] ; then
+  echo "Zipping up docs for offline download..."
+  cd builds/html
+  zip -r lucee-docs.zip *
+  cd ../../
+  cp -r builds/html builds/artefacts
+  echo "Preparing dash artifacts..."
+  mkdir builds/artefacts/dash
+  cp builds/dash/lucee.xml builds/artefacts/dash/
+  cd builds/dash
+  tar -cvzf ../../builds/artefacts/dash/lucee.tgz lucee.docset
+  cd ../../
+fi
