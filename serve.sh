ecec7aca613dbec8c87245459249620d9f36bc0d
AJ Mercer <andrew@webonix.net>
2015-10-15 13:27:32 +0800

    query member function examples that will run on trycf

--- serve.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd `dirname $0`
-CWD="`pwd`"
-
-box start name=luceedocslocalserver directory=$CWD/server/ force=true rewritesEnable=true rewritesConfig=$CWD/server/urlrewrite.xml port=4040 trayIcon=$CWD/luceelogoicon.png
