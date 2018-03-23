e28c0e37a5651b7b349c671b52747c6a5ee359fd
Zac Spitzer <zac.spitzer@gmail.com>
2018-03-23 15:32:54 +1100

    initial cut windows batch files

---
+++ build.bat
@@ -0,0 +1,56 @@
+set CWD=%cd%
+
+echo Starting Lucee5 server with which to build the docs...
+box server stop luceedocsbuilder
+box start ^
+name="luceedocsbuilder" ^
+cfengine="lucee@5" ^
+port=8765 ^
+openbrowser=false ^
+directory=%CWD% ^
+heapSize=1024
+
+echo Done!
+echo Importing reference docs from previously undocumented functions and tags...
+curl http://localhost:8765/import.cfm
+
+echo Building documentation (please be patient, it may take some time)...
+
+curl http://localhost:8765/build.cfm
+
+echo "Stopping Lucee5 server..."
+box server stop luceedocsbuilder
+@echo off
+IF EXIST .exitcode (
+  set /p exitcode=<.exitcode
+  del .exitcode
+  echo Exiting build, documentation build failed with exitcode %exitcode%
+  exit /b %exitcode%
+)
+echo on
+
+echo Exporting docs TBD for Windows batch file
+exit /b
+
+echo Building complete
+
+
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
+  echo "Zipping up docs for offline download..."
+  cd builds/html
+  cp ../../.cloudfront-distribution-id ./
+  zip -q -r lucee-docs.zip *
+  cd ../../
+  echo "Zipped."
+  echo "Preparing dash artifacts..."
+  cp -r builds/html builds/artifacts
+  mkdir builds/artifacts/dash
+  cp builds/dash/lucee.xml builds/artifacts/dash/
+  cd builds/dash
+  tar -czf ../../builds/artifacts/dash/lucee.tgz lucee.docset
+  cd ../../
+  echo "Prepared."
+  echo "Syncing with S3..."
+  s3_website push
+  echo "All done :)"
+fi
