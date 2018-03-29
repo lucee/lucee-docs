1d2c1fe3d8717a0326396b1f02c3a792d8c5d577
Zac Spitzer <zac.spitzer@gmail.com>
2018-03-29 18:33:33 +1100

    add suggestions to 404 page

--- builders/html/assets/js/src/uberCoolLuceeDocsSearch.js
+++ builders/html/assets/js/src/uberCoolLuceeDocsSearch.js
@@ -2,7 +2,7 @@
 
 	var $searchBox       = $( "#lucee-docs-search-input" )
 	  , $searchLink      = $( ".search-link" )
-	  , $searchContainer = $( ".search-container" )
+		, $searchContainer = $( ".search-container" )		
 	  , duckduckgoUrl    = "https://duckduckgo.com/?q=site:docs.lucee.org "
 	  , setupTypeahead, setupBloodhound, renderSuggestion
 	  , itemSelectedHandler, tokenizer, generateRegexForInput, search, searchIndex;
@@ -23,6 +23,10 @@
 
 			$searchBox.typeahead( typeAheadSettings, datasetSettings );
 			$searchBox.on( "typeahead:selected", function( e, result ){ itemSelectedHandler( result ); } );
+			
+			$fileNotFound = $(".file-not-found-suggestions")
+			if ($fileNotFound.length)
+				renderFileNotFoundSuggestions($fileNotFound);			
 		} );
 	};
 
@@ -117,5 +121,19 @@
 	}
 
 	setupTypeahead();
+	
+	// on the 404 page, try and make some suggestions based on filename
+	renderFileNotFoundSuggestions = function($fileNotFound){
+		var q = document.location.pathname.split(".")[0].split("/");		
+		var suggestions = 	search(q[q.length-1].split("-").join(" "));	
+
+		if (suggestions.length > 1)
+			$fileNotFound.append($("<p/>").text("Perhaps one of these pages is what your looking for?"));
+		for (var s = 0; s < suggestions.length; s++){
+			var item = suggestions[s];
+			var link = $("<a/>").text(item.display).attr("href", item.value.substr(1));			
+			$fileNotFound.append($("<div/>").append(link));
+		}
+	}		
 
 } )( jQuery );
