2c48199b871a8915efea48c11c4367c00590d4f8
Zac Spitzer <zac.spitzer@gmail.com>
2018-03-30 18:31:50 +1100

    don't show empty oembed icon when error/404

--- builders/html/assets/js/src/oembed.js
+++ builders/html/assets/js/src/oembed.js
@@ -1,12 +1,15 @@
 setTimeout(
     function(){
-        $(function(){           
+        $(function(){                       
             $(".content-inner .body a").each(function(){                
                 if ($(this).hasClass("edit-link") || $(this).hasClass("local-edit-link"))
                     return;
-                var url = $(this).attr("href");    
-                if (url && url.indexOf("http") === 0) // avoid local content!
-                    $(this).oembed();
+                var url = $(this).attr("href");                    
+                if (url && url.indexOf("http") === 0){ // avoid local content!
+                    $(this).oembed(url, {
+                        fallback : false
+                    });
+                }
             });       
         });
     }, 
