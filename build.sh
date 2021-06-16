81b7196baef64477fda1fd8cb56397ce59a28fb5
Zac Spitzer <zac.spitzer@gmail.com>
2021-06-16 14:26:32 +0200

    update build.sh for GitHub Actions

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 cd `dirname $0`
 CWD="`pwd`"
 
-echo "Starting Lucee5 server with which to build the docs..."
+echo "Starting Lucee server with which to build the docs..."
 box server stop luceedocsbuilder
 box start \
 name="luceedocsbuilder" \
@@ -14,12 +14,11 @@ directory=$CWD \
 javaVersion=openjdk8_jre_jdk8u262-b10 \
 heapSize=1024;
 
-echo "Done!";
+echo "Local Lucee Server Started!";
 echo "Importing reference docs from previously undocumented functions and tags..."
 curl --no-progress-meter http://localhost:8765/import.cfm?textlogs=true
 
 echo "Building documentation (please be patient, it may take some time)..."
-
 curl --no-progress-meter http://localhost:8765/build.cfm?textlogs=true
 
 echo "Stopping Lucee5 server..."
@@ -35,7 +34,7 @@ fi
 
 echo "Building complete"
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
+if [ "$DOCS_BRANCH" = "refs/heads/master" ] && [ "$DOCS_EVENT" = "push_request" ] ; then
   echo "Zipping up docs for offline download..."
   cd builds/html
   cp ../../.cloudfront-distribution-id ./
@@ -49,14 +48,14 @@ if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] ; the
   cd builds/dash
   tar -czf ../../builds/artifacts/dash/lucee.tgz lucee.docset
   cd ../../
-  echo "Prepared."
-  echo "Syncing with S3..."
-  s3_website push
-  echo "All done :)"
+  echo "Prepared for s3 upload."
+  # this is now done in a github action step
+  #echo "Syncing with S3..."
+  #s3_website push
+  #echo "All done :)"
 fi
 
-echo "Ping search engines with sitemaps"
-
-curl https://google.com/ping?sitemap=https://docs.lucee.org/sitemap.xml
+#echo "Ping search engines with sitemaps"
+#curl https://google.com/ping?sitemap=https://docs.lucee.org/sitemap.xml
 
 
